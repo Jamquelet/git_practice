76256305f26ee22589dd82c3f285756040d79f54
Jose Maya <jamquelet@hotmail.com>
2023-07-06 16:13:36 -0500

    CRUD: Create - Read - Update - Delete

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 _ejercicios practicos en python donde se repasan los fundamentos del lenguaje de programación_
 Elaborado por ***José Maya*** para la plataforma Ada School
 
-El archivo principal contiene el codigo de programa donde el user elije piedra, pápel o tijera frente a una elección aleatoria de la maquina.
+El archivo principal contiene el codigo de un programa donde el user elije piedra, pápel o tijera frente a una elección aleatoria de la maquina.
 
 ### Jerarquia Operadores aritméticos
 P - paréntesis
--- ejercicios/metodos_listas_crud.py
+++ ejercicios/metodos_listas_crud.py
@@ -10,4 +10,37 @@ print(numbers)
 numbers.insert(0, 'hello') #recive dos parametros la posicion y cual es mi elemento
 print(numbers)
 
-numbers.insert(3, 'hice un cambio')
+numbers.insert(3, 'hice un cambio') #corre los demas a la derecha no elimina
+print(numbers)
+
+#fucionar listas,unir una lista a otra 
+tasks = ['todo 1', 'todo 2', 'todo 3'] 
+new_list = numbers + tasks
+print(new_list) 
+
+#metodo index para preguntar en que posicion esta un elemento
+index = new_list.index('todo 2')
+new_list[index] = 'todo changed'
+print(new_list)
+
+#metodos para eliminar elementos de la lista
+new_list.remove('todo 1')
+print(new_list)
+
+new_list.pop()# ,pop elimina el ultimo elemento de la lista
+print(new_list)
+
+new_list.pop(0) #tambien se le puede dar la posicion del elemento
+print(new_list)
+
+new_list.reverse() #metodo para voltiar todo el array trasforma el array y lo cambia de posicion
+print(new_list)
+
+#sort ordenar
+numbers_a = [1, 4, 6, 3]
+numbers_a.sort() #ordena del menor al mayor
+print(numbers_a)
+
+strings = ['re', 'ab', 'ed'] 
+strings.sort() #ordena en el nombre del abecedario, si tiene tipos de datos mezclados no funciona
+print(strings)
