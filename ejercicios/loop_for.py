951216b418ce5bfadcf6f9f180a693c6510ddf72
Jose Maya <jamquelet@hotmail.com>
2023-07-07 22:42:31 -0500

    Se agrega funcionalidad al programa main

--- ejercicios/loop_for.py
+++ ejercicios/loop_for.py
@@ -8,3 +8,53 @@ for element in my_list:
 my_tuple = ('ja', 'an', 'bi', 'co', 'de', 'em')
 for element in my_tuple:
     print(element)
+
+product = {
+    'name': 'Shirt',
+    'price': 100,
+    'stock': 80
+}
+
+""" for element in product:
+    print(element) """ #imprime solo las llaves
+
+""" for element in product:
+    print(product[element]) """ #imprime solo los valores
+for key in product:
+    print(key, '=>', product[key])
+#otra forma de obtener la llave valor seria:
+for key, value in product.items(): #genera un array que lo vuelve tupla
+    print(key, '=>', value)
+
+people = [
+    {
+        'name': 'John',
+        'age': 21
+    },
+    {
+        'name': 'Zule',
+        'age': 29
+    },
+    {
+        'name': 'Santi',
+        'age': 44
+    }
+]
+#recorrer esta lista de personas que trae varios diccionarios
+for person in people:
+    print(person)
+
+for person in people:
+    print('name =>', person['name'])
+    
+#recorrer lista y seleccionar solo los numeros positivos y agregarlos a un nuevo array
+
+my_list = [20,-30,50,8,-8,1,-1,2,-2,3,-3,4,-4]
+new_list = []
+
+
+for n in my_list:
+    if not n < 0:
+        new_list.append(n)
+new_list.sort()
+print(new_list)
