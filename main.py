99de54e8999733a0b06efbd097cd03f5daab0ee9
Jose Maya <jamquelet@hotmail.com>
2023-07-15 13:53:38 -0500

    refactorizacion main - lambda

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import random
 
-computer_wins = 0
-user_wins = 0
-rounds = 1
-
 def choose_options():
     options = ('piedra', 'papel', 'tijera')
     user_option = input('piedra, papel o tijera => ')
@@ -12,7 +8,7 @@ def choose_options():
     if not user_option in options:
         print('esa opcion no es valida')
         #continue
-        return None, None
+        return None, None#cuando el user no seleccione una opcion valida 
 
     computer_option = random.choice(options)
 
@@ -20,19 +16,7 @@ def choose_options():
     print('Computer option => ', computer_option)
     return user_option, computer_option
 
-while True:
-
-    print('*' * 10)
-    print('Round', rounds)
-    print('*' * 10)
-
-    print('computer_wins', computer_wins)
-    print('user_wins', user_wins)
-
-    rounds += 1
-
-    user_option, computer_option = choose_options()
-
+def check_rules(user_option, computer_option, user_wins, computer_wins):
     if user_option == computer_option:
         print('Empate!')
     elif user_option == 'piedra':
@@ -62,15 +46,36 @@ while True:
             print('piedra gana a tijera')
             print('computer gano')
             computer_wins += 1
+    return user_wins, computer_wins
 
-    if computer_wins == 2:
-        print('*' *5)
-        print('El ganador es la computadora')
-        break
+def run_game():
+    computer_wins = 0
+    user_wins = 0
+    rounds = 1
 
-    if user_wins == 2:
-        print('*' *5)
-        print('El ganador es el usuario')
-        break
+    while True:
 
-    
+        print('*' * 10)
+        print('Round', rounds)
+        print('*' * 10)
+
+        print('computer_wins', computer_wins)
+        print('user_wins', user_wins)
+
+        rounds += 1
+
+        user_option, computer_option = choose_options()
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+
+        if computer_wins == 2:
+            print('*' *5)
+            print('El ganador es la computadora')
+            break
+
+        if user_wins == 2:
+            print('*' *5)
+            print('El ganador es el usuario')
+            break
+
+run_game()
---
+++ py102/lambda.py
@@ -0,0 +1,8 @@
+def increment (x):
+    return x+1
+
+result = increment(10)
+print(result)
+
+result2 = lambda x: x+1
+print(result2)
